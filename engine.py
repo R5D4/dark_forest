531caf7031be92fee1bebc1f02856a510e8460c8
R5D4 <dingrand@cse.yorku.ca>
2015-08-28 15:08:14 -0400

    Add docstring for engine module

--- engine.py
+++ engine.py
@@ -1,16 +1,15 @@
-# A game engine that uses information from a game map to allow the player to
-# progress through a game
+""" This module contains the Engine class (game engine)."""
 
 class Engine(object):
+    """ The game engine that gets and runs the next scene from the map."""
 
     def __init__(self, scene_map):
+        """ Initialize the engine with an existing game map."""
         self.scene_map = scene_map
 
     def play(self):
+        """ Start playing the game by entering the opening scene."""
         current_scene = self.scene_map.opening_scene()
-
         while True:
             next_scene_name = current_scene.enter()
             current_scene = self.scene_map.next_scene(next_scene_name)
-            
-            
