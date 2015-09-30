465040195d8ba22c56978ef3463a4dd88a8b1653
R5D4 <dingrand@cse.yorku.ca>
2015-09-30 17:00:11 -0400

    Implemented example map to see how it looks. Acceptable.

--- draw_map.py
+++ draw_map.py
@@ -3,5 +3,18 @@ Draw the map with ASCII characters.
 """
 
 def draw_test():
+    """ Quick test to see how the map would look."""
     bitmap = [
-                ['#
+                [' ', ' ', '#'],
+                [' ', ' ', '|'],
+                [' ', ' ', '#'],
+                [' ', '/', ' '],
+                ['#', ' ', ' ']
+             ]
+
+    map_string = ""
+    for row in bitmap:
+        for col in row:
+            map_string = "{}{}".format(map_string, col)
+        map_string = "{}\n".format(map_string)
+    print map_string
