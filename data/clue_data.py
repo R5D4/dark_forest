8f637cffeddf422a22b8634486c45480b1bcc0d8
R5D4 <dingrand@cse.yorku.ca>
2016-01-15 22:04:14 -0500

    Started populating clue data for printing out nice clue messages.

---
+++ data/clue_data.py
@@ -0,0 +1,13 @@
+""" 
+This module contains clue descriptions.
+"""
+
+# Footprint
+FOOTPRINT_DESC = {
+    'count_single': "There is a single track of prints.",
+    'count_multi': "There are several tracks of prints.",
+    'desc': "Each print is larger than a great bear's.",
+    'age_fresh': "There is a fresh track of prints.",
+    'age_old': "The track is old.",
+}
+    
